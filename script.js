3119d96c92e9f1fc8550dbd7656ca70311356326
eternalgratis <tyaps002@gmail.com>
2022-07-05 21:47:39 +0100

    Add Html

---
+++ script.js
@@ -0,0 +1,10 @@
+/* eslint max-classes-per-file: ["error", 3] */
+
+// Book Class: Represents a Book
+class Book {
+  constructor(title, author, size) {
+    this.title = title;
+    this.author = author;
+    this.size = size;
+  }
+}
