303cf28128d757cf869b253c9f7d9e8110301246
aron-helu <aronmilahela@gmail.com>
2022-07-06 10:12:20 +0300

    fixing linters error

--- script.js
+++ script.js
@@ -2,72 +2,72 @@
 
 // Book Class: Represents a Book
 class Book {
-    constructor(title, author) {
-        this.title = title;
-        this.author = author;
-    }
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
 }
 
 // Store Class: handles local storage
 class Store {
-    static getBooks() {
-        let books;
-        if (localStorage.getItem('books') === null) {
-            books = [];
-        } else {
-            books = JSON.parse(localStorage.getItem('books'));
-        }
-        return books;
+  static getBooks() {
+    let books;
+    if (localStorage.getItem('books') === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
     }
+    return books;
+  }
 
-    static addBook(book) {
-        const books = Store.getBooks();
-        books.push(book);
-        localStorage.setItem('books', JSON.stringify(books));
-    }
+  static addBook(book) {
+    const books = Store.getBooks();
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books));
+  }
 
-    static removeBook(author) {
-        const books = Store.getBooks();
-        books.forEach((book, index) => {
-            if (book.author === author) {
-                books.splice(index, 1);
-            }
-        });
-        localStorage.setItem('books', JSON.stringify(books));
-    }
+  static removeBook(author) {
+    const books = Store.getBooks();
+    books.forEach((book, index) => {
+      if (book.author === author) {
+        books.splice(index, 1);
+      }
+    });
+    localStorage.setItem('books', JSON.stringify(books));
+  }
 }
 
 // awesome Class: Handle UI Tasks
 class awesomeBooks {
-    static displayBooks() {
-        const books = Store.getBooks();
-        books.forEach((book) => awesomeBooks.addBookToList(book));
-    }
+  static displayBooks() {
+    const books = Store.getBooks();
+    books.forEach((book) => awesomeBooks.addBookToList(book));
+  }
 
-    static addBookToList(book) {
-        const list = document.getElementById('book-list');
-        const listBook = document.createElement('div');
-        listBook.classList.add('list-book')
-        listBook.innerHTML = `
+  static addBookToList(book) {
+    const list = document.getElementById('book-list');
+    const listBook = document.createElement('div');
+    listBook.classList.add('list-book');
+    listBook.innerHTML = `
             <p>"${book.title}"</p>
             <p>by</p>
             <p>${book.author}</p>
             <button class="delete">Remove</button>
         `;
 
-        list.appendChild(listBook);
-    }
+    list.appendChild(listBook);
+  }
 
-    static deleteBook(el) {
-        if (el.classList.contains('delete')) {
-            el.parentElement.remove();
-        }
+  static deleteBook(el) {
+    if (el.classList.contains('delete')) {
+      el.parentElement.remove();
     }
+  }
 
-    static clearField() {
-        document.querySelector('#title').value = '';
-        document.querySelector('#author').value = '';
-    }
+  static clearField() {
+    document.querySelector('#title').value = '';
+    document.querySelector('#author').value = '';
+  }
 }
 
 // Event: Display Books
@@ -75,29 +75,29 @@ document.addEventListener('DOMContentLoaded', awesomeBooks.displayBooks);
 
 // Event: Add a Book
 document.querySelector('#book-form').addEventListener('submit', (e) => {
-    // Prevent actual submit
-    e.preventDefault();
+  // Prevent actual submit
+  e.preventDefault();
 
-    // Get form values
-    const title = document.querySelector('#title').value;
-    const author = document.querySelector('#author').value;
+  // Get form values
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
 
-    // Instatiate book
-    const book = new Book(title, author);
+  // Instatiate book
+  const book = new Book(title, author);
 
-    // Add Book to UI
-    awesomeBooks.addBookToList(book);
+  // Add Book to UI
+  awesomeBooks.addBookToList(book);
 
-    // Store.addBook(book);
-    Store.addBook(book);
+  // Store.addBook(book);
+  Store.addBook(book);
 
-    // clear fields
-    awesomeBooks.clearField();
+  // clear fields
+  awesomeBooks.clearField();
 });
 
 // Event: Remove a Book
 document.querySelector('#book-list').addEventListener('click', (e) => {
-    awesomeBooks.deleteBook(e.target);
+  awesomeBooks.deleteBook(e.target);
 
-    Store.removeBook(e.target.previousElementSibling.textContent);
+  Store.removeBook(e.target.previousElementSibling.textContent);
 });
